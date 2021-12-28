b4ee58c3526d27bf14a4b6520c7a03db7a5d42a6
BigBIueWhale <rzyroff@gmail.com>
2021-12-28 23:02:40 +0200

    Add project files.

---
+++ rsa_cpp/rsa.cpp
@@ -0,0 +1,18 @@
+#include "sha512.hpp"
+
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+int main()
+{
+	constexpr const std::uint8_t message[] = "hello world";
+	const std::array<std::uint8_t, 512 / 8> hash_bytes = sha512::calculate_sha512_hash(message, sizeof(message) - 1);
+	for (const std::uint8_t& byte_elem : hash_bytes)
+	{
+		std::ostringstream num_as_str;
+		num_as_str << std::setw(2) << std::setfill('0') << std::hex << static_cast<int>(byte_elem);
+		std::cout << num_as_str.str() << " ";
+	}
+	return 0;
+}
