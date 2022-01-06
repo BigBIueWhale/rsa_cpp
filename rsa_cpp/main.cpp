8457d7f69980b24e6058201cd021a10542f380af
Natan Freeman <natanfreeman@outlook.com>
2022-01-06 15:46:26 +0200

    Adds the CMake build files to the .gitignore

--- rsa_cpp/main.cpp
+++ rsa_cpp/main.cpp
@@ -5,12 +5,14 @@
 int main()
 {
 	const std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
-	
+
 	std::random_device rd;
 	cryptb::random_engine engine(static_cast<boost::multiprecision::uint128_t>(rd.operator()()));
-	cryptb::rsa whatever{ engine };
-	std::cout << "e: " << whatever.get_e() << std::endl << " d: " << whatever.get_d() << std::endl << " N: " << whatever.get_N() << std::endl;
-	
+	cryptb::rsa whatever{engine};
+	std::cout << "e: " << whatever.get_e() << std::endl
+			  << " d: " << whatever.get_d() << std::endl
+			  << " N: " << whatever.get_N() << std::endl;
+
 	const std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 	const std::chrono::steady_clock::duration duration = end - start;
 	const long double seconds = std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count() / 1e9L;
